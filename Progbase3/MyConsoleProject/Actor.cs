7a8b3f6a59a2b7819ff57b2b86041e76a8d68e1f
gurug-prog <ivanmysterys@gmail.com>
2021-04-25 18:06:18 +0300

    fix: change type of age in Actor class

--- Progbase3/MyConsoleProject/Actor.cs
+++ Progbase3/MyConsoleProject/Actor.cs
@@ -4,8 +4,16 @@ namespace MyConsoleProject
     {
         public int id;
         public string fullName; // textFile
-        public byte age; // int in range 15 - 80
+        public int age; // int in range 15 - 80
         public string rolePlan; // leading, supporting, extra
+
+        public Actor()
+        {
+            id = 0;
+            fullName = "";
+            age = 0;
+            rolePlan = "";
+        }
 
         public Actor(int id, string fullName, byte age, string rolePlan)
         {
